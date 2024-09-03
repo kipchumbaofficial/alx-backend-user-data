b882ee88288e99315f74e18e8d617bff12607950
victor <victorkipchumba039@gmail.com>
2024-09-03 15:06:33 +0300

    Task 2: Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
         - unauthorized handler
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Test:
+        - forbidden handler
+    """
+    abort(403)
