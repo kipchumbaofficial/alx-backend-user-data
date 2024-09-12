aacc55cb40c2e12048b17983875eb493fdf91a68
victor <victorkipchumba039@gmail.com>
2024-09-12 07:20:26 +0300

    Task 2: find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 
 from user import Base, User
 
@@ -31,7 +34,28 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """Creates an new user in db
+        """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Finds user by given keyword arguments
+        """
+        try:
+            query = self._session.query(User)
+            valid_columns = set([column.name
+                                for column in User.__table__.columns])
+
+            for key, value in kwargs.items():
+                if key not in valid_columns:
+                    raise InvalidRequestError("Invalid filter key")
+                query = query.filter(getattr(User, key) == value)
+            user = query.one()
+            return user
+        except NoResultFound as e:
+            raise
+        except InvalidRequestError as ire:
+            raise
