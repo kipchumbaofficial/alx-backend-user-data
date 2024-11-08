9e444cfa8e334430a08ef2889bf81b6add371da1
victor <victorkipchumba039@gmail.com>
2024-11-08 05:55:11 +0300

    Task 0: Filter Datum pycodestyle correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,5 +10,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ obfuscated log messages """
     for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}', f'{field}={redaction}{separator}', message)
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
