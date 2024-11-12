ff794db43b24a5d2e7858dfbf9ea46deff5b973d
victor <victorkipchumba039@gmail.com>
2024-11-12 05:06:12 +0300

    Task 1: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> None:
+    """ GET /api/v1/unauthorized
+    raise 401
+    """
+    abort(401)
