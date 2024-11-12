d46dde391250909b458de0026aa3e10480f1cece
victor <victorkipchumba039@gmail.com>
2024-11-12 21:18:06 +0300

    Task 4: require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,14 @@ class Auth:
         """ require_auth:
         Returns: False
         """
-        return False
+        if not path or not excluded_paths:
+            return True
+        if not path.endswith("/"):
+            path += "/"
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header
