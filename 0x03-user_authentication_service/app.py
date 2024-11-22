0d7b8a79cfadbc23694093c6b47c2874a0499074
victor <victorkipchumba039@gmail.com>
2024-11-22 05:17:17 +0300

    task 17 get tokens

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -94,16 +94,18 @@ def profile():
 def get_reset_password_token():
     """ Gets rest password token
     """
-    email = request.form.get('email')
-    if email:
-        try:
-            token = AUTH.get_reset_password_token(email)
-            return jsonify({
-                "email": email,
-                "reset_token": token
-                }), 200
-        except ValueError:
-            abort(403)
+    try:
+        email = request.form.['email']
+    except KeyError:
+        abort(403)
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({
+            "email": email,
+            "reset_token": token
+            }), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
