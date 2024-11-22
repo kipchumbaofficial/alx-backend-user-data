0ce5770b9db959df1613767704422ac9ac023ef5
victor <victorkipchumba039@gmail.com>
2024-11-22 04:13:49 +0300

    task 11: Login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -32,6 +32,30 @@ def users():
         return jsonify({
             "message": "email already registered"
             }), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """ Login user by creating a session Id cookie
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+    is_loggedin = AUTH.valid_login(
+            request.form['email'],
+            request.form['password']
+            )
+    if is_loggedin:
+        session_id = AUTH.create_session(email)
+        response = jsonify({
+            "email": email,
+            "message": "logged in"
+            })
+        response.set_cookie("session_id", session_id)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
