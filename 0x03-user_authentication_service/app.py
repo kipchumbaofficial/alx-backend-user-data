51f26c7abfb1cdde3563610b78301b06ef80bce2
victor <victorkipchumba039@gmail.com>
2024-11-22 05:14:08 +0300

    task 17 get tokens

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,5 +90,21 @@ def profile():
     abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'])
+def get_reset_password_token():
+    """ Gets rest password token
+    """
+    email = request.form.get('email')
+    if email:
+        try:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify({
+                "email": email,
+                "reset_token": token
+                }), 200
+        except ValueError:
+            abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
