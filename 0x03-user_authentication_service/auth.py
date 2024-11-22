4ce27180da8ad044f29b8976fe883f75b93ecb0c
victor <victorkipchumba039@gmail.com>
2024-11-22 03:38:09 +0300

    Task 10: Session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,3 +52,15 @@ class Auth:
                     )
         except (InvalidRequestError, NoResultFound):
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Creates a session Id for a give user
+        """
+        session_id = None
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+        except (InvalidRequestError, NoResultFound):
+            return None
+        return session_id
